c57bd7c059a3cc08eecb3f7be17ccb8939d6e91c
RamaKrishna <64081163+krishponnaganti@users.noreply.github.com>
2021-08-22 23:07:42 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import "./styles.css";
-
-export default function App() {
+import { useRef, useEffect } from "react";
+function UserForm() {
+  let firstName = useRef(null);
+  let lastName = useRef(null);
+  let submit = useRef(null);
+  useEffect(() => {
+    firstName.current.focus();
+    console.log();
+  }, []);
+  function handleFirstname(e) {
+    if (e.key === "Enter") {
+      lastName.current.focus();
+    }
+  }
+  function handleLastname(e) {
+    if (e.key === "Enter") {
+      submit.current.focus();
+    }
+  }
+  function handleSubmit(e) {
+    console.log("wo");
+    e.preventDefault();
+  }
   return (
-    <div className="App">
-      <h1>ReactJs</h1>
-      <h2>useRef</h2>
-    </div>
+    <form>
+      <input type="text" ref={firstName} onKeyDown={handleFirstname} />
+      <input type="text" ref={lastName} onKeyDown={handleLastname} />
+      <input type="submit" ref={submit} onKeyDown={handleSubmit} />
+    </form>
   );
 }
+export default UserForm;
